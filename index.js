22788a2f924619da31998230779210240176b41b
Jim Hough <teachlife@outlook.com>
2020-06-02 14:43:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function findAdjacent(rootNode, vertices, edges){
         if( vertex !== rootNode){
           const connect = vertices.find(e => e.name = vertex)
           connect.distance = 0
-          console.log(connect)
           vert.push(connect)
+          console.log(vert)
         }
       }
     }
